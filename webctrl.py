1248c889f8691734d95f2a127a8f344872547d82
lastavengers <lastavengers@outlook.com>
2016-01-15 19:28:30 +0800

    update

--- webctrl.py
+++ webctrl.py
@@ -7,19 +7,22 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 cmd = {
-  #     '关机': 'poweroff',
-  #     '重启': 'reboot',
-        '重启 MentoHUST': 'systemctl restart mentohust',
+      # '关机': 'sudo poweroff',
+      # '重启': 'sudo reboot',
+      # '重启 MentoHUST': 'sudo systemctl restart mentohust',
         }
 
 @app.route('/', methods=['GET'])
 def index():
     action = request.args.get('action')
     if action:
-        system(cmd[action])
-        return render_template('index.html', message = '即将 ' + action)
+        print(cmd.keys())
+        if action in cmd.keys():
+            system(cmd[action])
+            return render_template('index.html', message = '即将 ' + action)
+        return render_template('index.html', message = '不支持的操作')
     else:
         return render_template('index.html', message = 'Raspi WebCtrl')
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0')
+    app.run(debug = True, host = '0.0.0.0', port = 2333)
